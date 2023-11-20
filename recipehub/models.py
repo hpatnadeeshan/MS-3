338929e37b2325ce72d1e983f9a19e311a448b04
hpatnadeeshan <>
2023-11-20 13:36:35 +0000

    added  Recipe class to models.py

--- recipehub/models.py
+++ recipehub/models.py
@@ -6,3 +6,17 @@ class Cuisine(db.Model):
 
     def __repr__(self):
         return self.cuisine_name
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(255), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    preparation_steps = db.Column(db.Text, nullable=False)
+    image_link = db.Column(db.String(255))
+
+    # Foreign keys
+    cuisine_id = db.Column(db.Integer, db.ForeignKey('cuisine.id'), nullable=False)
+    cuisine = db.relationship('Cuisine', backref=db.backref('recipes', lazy=True))
+
+    def __repr__(self):
+        return "Recipe {}: {}".format(self.id, self.recipe_name)
