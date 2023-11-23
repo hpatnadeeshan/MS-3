e30af0cc3c6cca18b1d9aed79e8f1e4be9e90dbb
hpatnadeeshan <>
2023-11-23 20:09:35 +0000

    Define the route for displaying a recipe & send is_index parameter

--- recipehub/routes.py
+++ recipehub/routes.py
@@ -10,4 +10,12 @@ def home():
     recipes = Recipe.query.all()
     # print(recipes[1])
     random_recipes = random.sample(recipes, 4)
-    return render_template('index.html', recipes=random_recipes)
+    return render_template('index.html', recipes=random_recipes,is_index=True)
+
+
+# Define the route for displaying a recipe
+@app.route('/recipe/<int:recipe_id>')
+def view_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+
+    return render_template('recipe.html', recipe=recipe)
