4bec6eded74789bdbb610049bcf9e35189ac2fd0
hpatnadeeshan <>
2023-11-23 16:43:08 +0000

    Update Flask route to fetch and display random recipes

--- recipehub/routes.py
+++ recipehub/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from recipehub import app, db
 from recipehub.models import Cuisine, Recipe, Tools,RecipeTool
+import random
+
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    recipes = Recipe.query.all()
+    # print(recipes[1])
+    random_recipes = random.sample(recipes, 4)
+    return render_template('index.html', recipes=random_recipes)
