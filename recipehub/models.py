d53ac26903784b09c12d5ad1f32f9a08e8d8132c
hpatnadeeshan <>
2023-12-03 16:13:34 +0000

    added methods is_authenticated, is_active, is_anonymous, and get_id

--- recipehub/models.py
+++ recipehub/models.py
@@ -55,3 +55,16 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def is_authenticated(self):
+        return True  # Assuming all users are authenticated
+
+    def is_active(self):
+        return True  # Assuming all users are active
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
