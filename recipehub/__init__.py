47a16ad67c4d0ecefcce1ecfec9ed6d7fa1758d9
hpatnadeeshan <>
2023-12-03 10:58:56 +0000

    add login maganer app

--- recipehub/__init__.py
+++ recipehub/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 if os.path.exists("env.py"):
     import env  # noqa
@@ -10,5 +11,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from recipehub import routes  # noqa
