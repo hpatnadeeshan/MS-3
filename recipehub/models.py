f571fe72ed74db42de180b55bbf23a32c302c1d4
hpatnadeeshan <>
2023-12-09 01:54:52 +0000

    validated models.py

--- recipehub/models.py
+++ recipehub/models.py
@@ -2,12 +2,14 @@ from recipehub import db
 from flask_login import UserMixin
 from datetime import datetime
 
+
 class Cuisine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cuisine_name = db.Column(db.String(255), nullable=False, unique=True)
 
     def __repr__(self):
         return self.cuisine_name
+
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,13 +19,16 @@ class Recipe(db.Model):
     image_link = db.Column(db.String(255))
 
     # Foreign keys
-    cuisine_id = db.Column(db.Integer, db.ForeignKey('cuisine.id'), nullable=False)
-    cuisine = db.relationship('Cuisine', backref=db.backref('recipes', lazy=True))
+    cuisine_id = db.Column(
+        db.Integer, db.ForeignKey('cuisine.id'), nullable=False)
+    cuisine = db.relationship(
+        'Cuisine', backref=db.backref('recipes', lazy=True))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('recipes', lazy=True))
 
-    recipe_tool = db.relationship('RecipeTool', backref='recipe', lazy=True, cascade='all, delete-orphan')
-
+    recipe_tool = db.relationship(
+        'RecipeTool', backref='recipe',
+        lazy=True, cascade='all, delete-orphan')
 
     def __repr__(self):
         return "Recipe {}: {}".format(self.id, self.recipe_name)
@@ -34,33 +39,32 @@ class Tools(db.Model):
     tool_name = db.Column(db.String(255), nullable=False)
     brand_name = db.Column(db.String(255))
 
-    recipe_tool2 = db.relationship('RecipeTool', backref='tools', lazy=True, cascade='all, delete-orphan')
-    
+    recipe_tool2 = db.relationship(
+        'RecipeTool', backref='tools', lazy=True, cascade='all, delete-orphan')
+
     def __repr__(self):
-        return "Tool {}: {} (Brand: {})".format(self.id, self.tool_name, self.brand_name)
+        return "Tool {}: {} (Brand: {})".format(
+            self.id, self.tool_name, self.brand_name)
 
 
 class RecipeTool(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     # Foreign keys
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe_id = db.Column(
+        db.Integer, db.ForeignKey('recipe.id'), nullable=False)
     tool_id = db.Column(db.Integer, db.ForeignKey('tools.id'), nullable=False)
 
-    # Relationships
-    # recipe = db.relationship('Recipe', backref=db.backref('recipe_tools', lazy=True))
-    # tool = db.relationship('Tools', backref=db.backref('recipe_tools', lazy=True))
-
     def __repr__(self):
-        return "RecipeTool {}: Recipe {}, Tool {}".format(self.id, self.recipe_id, self.tool_id)
+        return "RecipeTool {}: Recipe {}, Tool {}".format(
+            self.id, self.recipe_id, self.tool_id)
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    is_admin = db.Column(db.Boolean,nullable=False, default=False)
-
-    # recipes = db.relationship('Recipe', backref='user', lazy=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
     def is_authenticated(self):
         return True  # Assuming all users are authenticated
@@ -79,6 +83,7 @@ class User(db.Model, UserMixin):
             self.id, self.username, self.password, self.is_admin
         )
 
+
 class ContactSubmission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
@@ -88,4 +93,8 @@ class ContactSubmission(db.Model):
     submitted_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"ContactSubmission(id={self.id}, name={self.name}, email={self.email}, submitted_at={self.submitted_at})"
+        return (
+            f"ContactSubmission(id={self.id}, "
+            f"name={self.name}, email={self.email}, "
+            f"submitted_at={self.submitted_at})"
+        )
